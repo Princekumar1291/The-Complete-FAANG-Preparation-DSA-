45760e83d2f689ae710c5dd6a1daff54f6cd4083
Prince kumar <princekumar7320918928@gmail.com>
2024-06-05 14:05:52 +0530

    13. Roman to Integer

---
+++ _05_String/_02_MediumStringProblems/_03_romanToInteger.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int helper(char ch){
+  if(ch=='I') return 1;
+  else if(ch=='V') return 5;
+  else if(ch=='X') return 10;
+  else if(ch=='L') return 50;
+  else if(ch=='C') return 100;
+  else if(ch=='D') return 500;
+  else if(ch=='M') return 1000;
+  return 0;
+}
+int romanToInt(string str) {
+  int n=str.size();
+  int ans=0;
+  for(int i=0;i<n-1;i++){
+      int temp1=helper(str[i]);
+      int temp2=helper(str[i+1]);
+      if(temp1>=temp2) ans+=temp1;
+      else ans-=temp1;
+  }
+  ans+=helper(str[n-1]);
+  return ans;
+}
+
+int main(){
+  
+  return 0;
+}

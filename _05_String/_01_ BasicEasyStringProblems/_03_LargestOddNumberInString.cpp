dbbda21f872e77c3ed8f1a81c06197798bbad07a
Prince kumar <princekumar7320918928@gmail.com>
2024-06-05 11:53:36 +0530

    1903. Largest Odd Number in String

---
+++ _05_String/_01_ BasicEasyStringProblems/_03_LargestOddNumberInString.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string largestOddNumber(string num) {
+  while(num.size()>0){
+    int t=num.back()-'0';
+    if(t%2==0) num.pop_back();
+    else return num;
+  }
+  return "";
+}
+
+int main(){
+  
+  return 0;
+}

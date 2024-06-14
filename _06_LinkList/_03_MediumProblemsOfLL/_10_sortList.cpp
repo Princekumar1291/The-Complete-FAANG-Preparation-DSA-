b984b44abfff7e563910394ca3bcd22806dfd02a
Prince kumar <princekumar7320918928@gmail.com>
2024-06-14 08:16:44 +0530

    Sort LinkList my mergeSort approch

--- _06_LinkList/_03_MediumProblemsOfLL/_10_sortList.cpp
+++ _06_LinkList/_03_MediumProblemsOfLL/_10_sortList.cpp
@@ -19,3 +19,60 @@ public:
         return head;
     }
 };
+
+
+
+
+
+
+class Solution {
+public:
+    ListNode* midNodeFun(ListNode* head){
+        if(head==NULL || head->next==NULL) return head;
+        ListNode* slow=head;
+        ListNode* fast=head->next;
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow;
+    }
+    ListNode* helper(ListNode* left,ListNode* right){
+
+        ListNode* newHead=new ListNode(-1);  //Dumy Node
+        ListNode* temp=newHead;
+        while(left!=NULL && right!=NULL){
+            if(left->val<right->val){
+                temp->next=left;
+                temp=left;
+                left=left->next;
+            }else{
+                temp->next=right;
+                temp=right;
+                right=right->next;
+            }
+        }
+        
+        while(left!=NULL){
+            temp->next=left;
+            temp=left;
+            left=left->next;
+        }
+        while(right!=NULL){
+            temp->next=right;
+            temp=right;
+            right=right->next;
+        }
+        return newHead->next;
+    }
+    ListNode* sortList(ListNode* head) {
+        if(head==NULL || head->next==NULL) return head;
+        ListNode* midNode=midNodeFun(head);
+        ListNode* left=head;
+        ListNode* right=midNode->next;
+        midNode->next=NULL;
+        left=sortList(left);
+        right=sortList(right);
+        return helper(left,right);
+    }
+};

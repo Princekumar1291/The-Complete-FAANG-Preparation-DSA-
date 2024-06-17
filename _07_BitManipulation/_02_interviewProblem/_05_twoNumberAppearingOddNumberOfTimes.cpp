39557ad23f12e2f08a492e5917c614bf41bfe508
Prince kumar <princekumar7320918928@gmail.com>
2024-06-17 11:48:25 +0530

    Two numbers with odd occurrences

--- _07_BitManipulation/_02_interviewProblem/_05_twoNumberAppearingOddNumberOfTimes.cpp
+++ _07_BitManipulation/_02_interviewProblem/_05_twoNumberAppearingOddNumberOfTimes.cpp
@@ -28,6 +28,31 @@ vector<long long int> twoOddNum(long long int Arr[], long long int N) {
 }
 
 
+vector<long long int> twoOddNum(long long int Arr[], long long int n){
+    long long xr=0;
+    for(int i=0;i<n;i++){
+        xr=xr^Arr[i];
+    }
+    long long xr0=xr&(~(xr-1));
+    long long xr1=0;
+    long long xr2=0;
+    for(int i=0;i<n;i++){
+        if((xr0&Arr[i])!=0) xr1=xr1^Arr[i]; 
+        else xr2=xr2^Arr[i];
+    }
+    vector<long long int> v;
+    if(xr1>xr2){
+        v.push_back(xr1);
+        v.push_back(xr2);
+    }
+    else{
+        v.push_back(xr2);
+        v.push_back(xr1);
+    }
+    return v;
+}
+
+
 int main(){
   
   return 0;

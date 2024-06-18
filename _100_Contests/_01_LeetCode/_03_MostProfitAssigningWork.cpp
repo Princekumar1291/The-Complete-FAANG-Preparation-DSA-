a7da7371b44f5ea4fea8bd9b45fc53e7be28dc59
Prince kumar <princekumar7320918928@gmail.com>
2024-06-18 11:33:41 +0530

    826. Most Profit Assigning Work

---
+++ _100_Contests/_01_LeetCode/_03_MostProfitAssigningWork.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int maxProfitAssignment(vector<int>& d, vector<int>& p, vector<int>& w) {
+    vector<pair<int,int>> v;
+    for(int i=0;i<d.size();i++){
+        v.push_back({d[i],p[i]});
+    }
+    sort(v.begin(),v.end());
+    int ans=0;
+
+    for(int i=0;i<w.size();i++){
+        int temp=0;
+        for(int j=0;j<v.size();j++){
+            if(v[j].first<=w[i]) {
+                temp=max(temp,v[j].second);
+            }
+            else{
+                break;
+            }
+        }
+        ans=ans+temp;
+    }
+    return ans;
+}
+
+
+int main(){
+  
+  return 0;
+}

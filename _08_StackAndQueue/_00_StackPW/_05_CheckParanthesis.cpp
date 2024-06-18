5b43eecc8b5f9f6804c83f1ed128384726a521b1
Prince kumar <princekumar7320918928@gmail.com>
2024-06-18 13:44:40 +0530

    20. Valid Parentheses

---
+++ _08_StackAndQueue/_00_StackPW/_05_CheckParanthesis.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isValid(string s) {
+  stack<char> st;
+  int n=s.size();
+  for(int i=0;i<n;i++){
+    if(s[i]=='(' || s[i]=='{'|| s[i]=='['){
+        st.push(s[i]);
+    }
+    else{
+      if(st.empty()){
+          return false;
+      }
+      if(s[i]==')' &&st.top()=='('){
+          st.pop();
+      }
+      else if(s[i]=='}' &&st.top()=='{'){
+          st.pop();
+      }
+      else if(s[i]==']' &&st.top()=='['){
+          st.pop();
+      }
+      else return false;
+    }
+  }
+  return st.empty();
+}
+
+
+int main(){
+  
+  return 0;
+}

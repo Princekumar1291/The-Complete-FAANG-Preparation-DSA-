c1e64def694c88cb4a66c25156303a53766be0e4
Prince kumar <princekumar7320918928@gmail.com>
2024-06-19 17:01:42 +0530

    Infix to Prefix

---
+++ _08_StackAndQueue/_00_StackPW/_14InfixToPrefix.cpp
@@ -0,0 +1,52 @@
+// Prefix expression (2+6*4/8-3) it is all about operator
+//In -->v1 op v2
+//Pre -->op v1 v2
+//Post -->v1 v2 op
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int opWeight(char ch){
+  if(ch=='+' || ch=='-') return 1;
+  return 2;
+}
+int main(){
+  string s="2+6*4/8-3";
+  int n=s.size();
+  stack<string> val;
+  stack<char> op;
+  for(int i=0;i<n;i++){
+    if(isdigit(s[i])){
+      string temp="";
+      temp+=s[i];
+      val.push(temp);
+    }
+    else{
+      if(op.empty()){
+        op.push(s[i]);
+      }
+      else if(opWeight(s[i])>opWeight(op.top())){
+        op.push(s[i]);
+      }
+      else{
+        while(!op.empty() && opWeight(s[i])<=opWeight(op.top())){
+          string s2=val.top();val.pop();
+          string s1=val.top();val.pop();
+          char c=op.top();op.pop();
+          string ans=c+s1+s2;
+          val.push(ans);
+        }
+        op.push(s[i]);
+      }
+    }
+  }
+  while(!op.empty()){
+    string s2=val.top();val.pop();
+    string s1=val.top();val.pop();
+    char c=op.top();op.pop();
+    string ans=c+s1+s2;
+    val.push(ans);
+  }
+  cout<<val.top();
+  return 0;
+}

53026fe74cc3964cbfba4478077a4a82cb34b58f
Prince kumar <princekumar7320918928@gmail.com>
2024-06-20 23:17:26 +0530

    Deque

---
+++ _08_StackAndQueue/_00_zQueuePW/_05_Deque.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main(){
+  deque<int> dq;
+  dq.push_back(10);   
+  dq.push_front(5);
+  dq.front();
+  dq.back();
+  dq.pop_back();
+  dq.pop_front();
+
+  return 0;
+}

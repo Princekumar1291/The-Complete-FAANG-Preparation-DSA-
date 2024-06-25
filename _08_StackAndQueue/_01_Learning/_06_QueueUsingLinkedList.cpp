fd7b16caaeb4b922a2be7651bc4a8d2ad3999b24
Prince kumar <princekumar7320918928@gmail.com>
2024-06-25 18:35:06 +0530

    Implement Queue using Linked List

---
+++ _08_StackAndQueue/_01_Learning/_06_QueueUsingLinkedList.cpp
@@ -0,0 +1,108 @@
+// Problem link: https://www.geeksforgeeks.org/problems/implement-queue-using-linked-list/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=implement-queue-using-linked-list
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct QueueNode
+{
+    int data;
+    QueueNode *next;
+    QueueNode(int a)
+    {
+        data = a;
+        next = NULL;
+    }
+};
+
+struct MyQueue {
+    QueueNode *front;
+    QueueNode *rear;
+    void push(int);
+    int pop();
+    MyQueue() {front = rear = NULL;}
+};
+
+
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        MyQueue *sq = new MyQueue();
+
+        int Q;
+        cin>>Q;
+        while(Q--){
+        int QueryType=0;
+        cin>>QueryType;
+        if(QueryType==1)
+        {
+            int a;
+            cin>>a;
+            sq->push(a);
+        }else if(QueryType==2){
+            cout<<sq->pop()<<" ";
+
+        }
+        }
+        cout<<endl;
+    }
+    }
+
+// } Driver Code Ends
+
+
+/* Structure of a node in Queue
+struct QueueNode
+{
+    int data;
+    QueueNode *next;
+    QueueNode(int a)
+    {
+        data = a;
+        next = NULL;
+    }
+};
+
+And structure of MyQueue
+struct MyQueue {
+    QueueNode *front;
+    QueueNode *rear;
+    void push(int);
+    int pop();
+    MyQueue() {front = rear = NULL;}
+}; */
+
+//Function to push an element into the queue.
+void MyQueue:: push(int x)
+{
+        // Your Code
+        QueueNode* newNode=new QueueNode(x);
+        if(rear==NULL){
+            front=rear=newNode;
+        }
+        else{
+            rear->next=newNode;
+            rear=newNode;
+        }
+}
+
+//Function to pop front element from the queue.
+int MyQueue :: pop()
+{
+        // Your Code  
+        if(front==NULL) return -1;
+        else if(front->next==NULL){
+            int ans=front->data;
+            front=rear=NULL;
+            return ans;
+        }
+        else {
+            int ans=front->data;
+            front=front->next;
+            return ans;
+        }
+}

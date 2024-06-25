52344047813761cd064246034b5102200fd9c909
Prince kumar <princekumar7320918928@gmail.com>
2024-06-25 18:22:03 +0530

    Implement Stack using Linked List

---
+++ _08_StackAndQueue/_01_Learning/StackUsingLL.cpp
@@ -0,0 +1,69 @@
+// Problem Link: https://www.geeksforgeeks.org/problems/implement-stack-using-linked-list/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=implement-stack-using-linked-list
+
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct StackNode {
+    int data;
+    StackNode *next;
+    StackNode(int a) {
+        data = a;
+        next = NULL;
+    }
+};
+
+class MyStack {
+  private:
+    StackNode *top;
+
+  public:
+    void push(int);
+    int pop();
+    MyStack() { top = NULL; }
+};
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        MyStack *sq = new MyStack();
+
+        int Q;
+        cin >> Q;
+        while (Q--) {
+            int QueryType = 0;
+            cin >> QueryType;
+            if (QueryType == 1) {
+                int a;
+                cin >> a;
+                sq->push(a);
+            } else if (QueryType == 2) {
+                cout << sq->pop() << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+// } Driver Code Ends
+
+
+//Function to push an integer into the stack.
+void MyStack ::push(int x) {
+    StackNode* newNode=new StackNode(x);
+    if(top==NULL) top=newNode;
+    else{
+        newNode->next=top;
+        top=newNode;
+    }
+}
+
+//Function to remove an item from top of the stack.
+int MyStack ::pop() {
+    if(top==NULL) return -1;
+    int ans=top->data;
+    top=top->next;
+    return ans;
+}

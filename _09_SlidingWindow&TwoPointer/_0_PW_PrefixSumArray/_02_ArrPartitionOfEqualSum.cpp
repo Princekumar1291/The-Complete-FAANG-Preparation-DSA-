4879f1be1013a6665bd9c1f2fc9662d72ccdf508
Prince kumar <princekumar7320918928@gmail.com>
2024-06-27 10:11:47 +0530

    Array partition of equal sum

---
+++ _09_SlidingWindow&TwoPointer/_0_PW_PrefixSumArray/_02_ArrPartitionOfEqualSum.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  vector<int> v={9,8,7,6,5,5,6,7,8,9};
+  int n=v.size();
+  vector<int> prefSum(n);
+  int sum=0;
+  for(int i=0;i<n;i++){
+    sum+=v[i];
+    prefSum[i]=sum;
+  }
+
+  for(int i=0;i<n-1;i++){
+    if(prefSum[i]==(prefSum[n-1]-prefSum[i])){
+      cout<<"Yes"<<endl;
+      cout<<prefSum[i]<<" "<<prefSum[n-1]-prefSum[i];
+      break;
+    }
+  }
+  return 0;
+}

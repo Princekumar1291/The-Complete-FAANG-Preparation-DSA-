d59f29cb37a97ce351d810052d30ebb478437064
Prince kumar <princekumar7320918928@gmail.com>
2024-07-01 23:26:30 +0530

    144. Binary Tree Preorder Traversal

---
+++ _10_BinaryTree/_01_PW/_08_PreOrderTraversal.cpp
@@ -0,0 +1,16 @@
+//Problem Link: https://leetcode.com/problems/binary-tree-preorder-traversal/description/
+
+class Solution {
+public:
+    void helper(TreeNode* root,vector<int>& ans){
+        if(root==NULL) return ;
+        ans.push_back(root->val);
+        helper(root->left,ans);
+        helper(root->right,ans);
+    }
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        helper(root,ans);
+        return ans;
+    }
+};

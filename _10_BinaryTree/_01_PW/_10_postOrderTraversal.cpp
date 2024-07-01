4280acdc6cc3cef88fb066e3d973a80228c92c32
Prince kumar <princekumar7320918928@gmail.com>
2024-07-01 23:35:28 +0530

    145. Binary Tree Postorder Traversal

---
+++ _10_BinaryTree/_01_PW/_10_postOrderTraversal.cpp
@@ -0,0 +1,18 @@
+//Problem Link: https://leetcode.com/problems/binary-tree-postorder-traversal/description/
+
+
+
+class Solution {
+public:
+    void helper(TreeNode* root,vector<int>& ans){
+        if(root==NULL) return ;
+        helper(root->left,ans);
+        helper(root->right,ans);
+        ans.push_back(root->val);
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        helper(root,ans);
+        return ans;
+    }
+};

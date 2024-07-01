189a656d019dcc0b47fed095b2f95e7f748582ad
Prince kumar <princekumar7320918928@gmail.com>
2024-07-01 08:30:16 +0530

    Tree basic

---
+++ _10_BinaryTree/_01_PW/_01_basic.cpp
@@ -0,0 +1,27 @@
+//Root Node
+//Children
+//Parent Node
+//Sibling Node
+//Leaf Node--Having No child
+//Internal Node -Not in boundry
+//Ancestor --> Path from root to that Node
+// Descendant Node --> all Node below that Node
+//Level
+//size --> Number of Nodes
+//edges --> size-1 (is that joing two node)
+// Height --> level-1
+
+//Traversing a tree
+  // 1)DFS 
+  // 2)BFS
+
+// Important properties of trees 
+  // .It has no loop and no circuit
+  // .It has no self loop
+
+
+
+//Generic tree --> multiple children
+//Binary tree  --> max two children(0,1,2)
+//Binary search tree-> node to left is all smaller and node to the right is larger
+//AVL tree--> balanced BST

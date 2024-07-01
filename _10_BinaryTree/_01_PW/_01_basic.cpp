ea40d2f733113352c0e37e89fc8742ebdcb134a3
Prince kumar <princekumar7320918928@gmail.com>
2024-07-01 13:19:54 +0530

    Basic BT

--- _10_BinaryTree/_01_PW/_01_basic.cpp
+++ _10_BinaryTree/_01_PW/_01_basic.cpp
@@ -25,3 +25,45 @@
 //Binary tree  --> max two children(0,1,2)
 //Binary search tree-> node to left is all smaller and node to the right is larger
 //AVL tree--> balanced BST
+
+
+
+//Binary Tree Types
+//01) Full BT-->every node have either 0 or 2 children
+    //     1
+    //    / \
+    //   2   3
+    //  / \
+    // 4   5
+
+//02) Perfect BT--> every node has 0 or 2 children and all leaf node are at same level
+    //     1        Not this      1   
+    //    / \                    / \
+    //   2   3                  2   3
+    //  / \ / \                / \ /
+    // 4  5 6  7              4  5 6
+
+//03) Complete BT--> all levels are completly filed except the last level but last level is start filling with left most
+    //      1     but not this    1
+    //     / \                   / \
+    //    2   3                 2   3
+    //   / \  /                /   / \
+    //  4   5 6               4   5   6
+    //                            \
+    //                             7
+
+//04) Balanced BT--> height of tree at max log(n) where n is number of nodes
+                   //In a balanced binary tree, the height of the tree is balanced such that the difference in height between the left and right subtrees of any node is at most one.
+      
+ 
+//05) Degenerate and skewed BT-->A degenerate binary tree (or pathological tree) is a type of binary tree where each parent node has only one child. This tree essentially behaves like a linked list because every node only points to one other node.
+    // 1
+    //  \
+    //   2
+    //    \
+    //     3
+    //      \
+    //       4
+
+
+

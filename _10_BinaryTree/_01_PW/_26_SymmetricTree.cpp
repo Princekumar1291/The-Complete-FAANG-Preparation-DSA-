6366533c31b69b78b4e7ee4f9a64e8d5b1038efe
Prince kumar <princekumar7320918928@gmail.com>
2024-07-03 15:51:26 +0530

    101. Symmetric Tree

--- _10_BinaryTree/_01_PW/_26_SymmetricTree.cpp
+++ _10_BinaryTree/_01_PW/_26_SymmetricTree.cpp
@@ -11,3 +11,27 @@ public:
         return ans;
     }
 };
+
+
+class Solution {
+public:
+    void helper(TreeNode* p,TreeNode* q,bool& ans){
+        if(p==NULL && q==NULL) return ;
+        else if(p==NULL || q==NULL) {
+            ans=false;
+            return ;
+        }
+        else if(p->val!=q->val){
+            ans=false;
+            return ;
+        }
+        helper(p->left,q->right,ans);
+        helper(p->right,q->left,ans);
+        return ;
+    }
+    bool isSymmetric(TreeNode* root) {
+        bool ans=true;
+        helper(root->left,root->right,ans);
+        return ans;
+    }
+};

904c3f4f140f1b219ae0522a00c16f4a32cc35d6
Prince kumar <princekumar7320918928@gmail.com>
2024-07-03 10:33:31 +0530

    Path Sum III

--- _10_BinaryTree/_01_PW/_22_PathSumIII.cpp
+++ _10_BinaryTree/_01_PW/_22_PathSumIII.cpp
@@ -17,21 +17,21 @@ class Node{
 
 class Solution {
 public:
-    void hleper1(TreeNode* root,long long& targetSum,long long sum,long long& ans){
+    void hleper1(Node* root,long long& targetSum,long long sum,long long& ans){
         if(root==NULL) return ;
         sum+=root->val;
         if(targetSum==sum) ans++;
         hleper1(root->left,targetSum,sum,ans);
         hleper1(root->right,targetSum,sum,ans);
     }
-    void helper2(TreeNode* root, long long& targetSum,long long& ans){
+    void helper2(Node* root, long long& targetSum,long long& ans){
         long long sum=0;
         if(root==NULL) return ;
         hleper1(root,targetSum,sum,ans);
         helper2(root->left,targetSum,ans);
         helper2(root->right,targetSum,ans);
     }
-    int pathSum(TreeNode* root, long long targetSum) {
+    int pathSum(Node* root, long long targetSum) {
         long long ans=0;
         helper2(root,targetSum,ans);
         return ans;

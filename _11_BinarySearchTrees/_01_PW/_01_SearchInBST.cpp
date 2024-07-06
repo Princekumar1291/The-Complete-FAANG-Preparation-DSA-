6ac0b3d21171df5398ec224de26e76a2a06a2771
Prince kumar <princekumar7320918928@gmail.com>
2024-07-06 14:10:59 +0530

    700. Search in a Binary Search Tree

---
+++ _11_BinarySearchTrees/_01_PW/_01_SearchInBST.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(root==NULL) return NULL;
+        if(root->val==val) return root;
+        else if(root->val>val) return searchBST(root->left,val);
+        else return searchBST(root->right,val);
+        return NULL;
+    }
+};
+
+
+
+
+
+
+
+class Solution {
+public:
+    void helper(TreeNode* root,TreeNode*& ans,int val){
+        if(root==NULL) return ;
+        if(root->val==val) {
+            ans=root;
+            return ;
+        }
+        else if(root->val>val) helper(root->left,ans,val);
+        else helper(root->right,ans,val);
+    }
+    TreeNode* searchBST(TreeNode* root, int val) {
+        TreeNode* ans=NULL;
+        helper(root,ans,val);
+        return ans;
+    }
+};

eef98c1466ddb6492333cd765821f4d68af53f57
Prince kumar <princekumar7320918928@gmail.com>
2024-07-08 15:55:08 +0530

    114. Flatten Binary Tree to Linked List

--- _11_BinarySearchTrees/_01_PW/_11_FlattenBTLinkedList.cpp
+++ _11_BinarySearchTrees/_01_PW/_11_FlattenBTLinkedList.cpp
@@ -16,3 +16,22 @@ public:
         helper(root,prev);
     }
 }; 
+
+
+
+
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        if(root==NULL) return ;
+        stack<TreeNode*> st;
+        st.push(root);
+        while(!st.empty()){
+            TreeNode* temp=st.top();st.pop();
+            if(temp->right!=NULL) st.push(temp->right);
+            if(temp->left!=NULL) st.push(temp->left);
+            if(!st.empty())temp->right=st.top();
+            temp->left=NULL;
+        }
+    }
+};

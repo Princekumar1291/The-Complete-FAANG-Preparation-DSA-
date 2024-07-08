c08f88c6711d773b3e58c29a99ddaf437e75616e
Prince kumar <princekumar7320918928@gmail.com>
2024-07-08 15:04:38 +0530

    Morries Preorder,Inorder

--- _11_BinarySearchTrees/_01_PW/_10_morriesTraversal.cpp
+++ _11_BinarySearchTrees/_01_PW/_10_morriesTraversal.cpp
@@ -1,6 +1,7 @@
 //Problem Link: https://leetcode.com/problems/binary-tree-inorder-traversal/description/
 
 
+//Inorder
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
@@ -19,6 +20,41 @@ public:
                 if(pred->right==curr){
                     pred->right=NULL;
                     ans.push_back(curr->val);
+                    curr=curr->right;
+                }
+            }
+            else{
+                ans.push_back(curr->val);
+                curr=curr->right;
+            }
+        }
+        return ans;
+    }
+};
+
+
+
+
+
+// Preorder
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        TreeNode* curr=root;
+        vector<int> ans;
+        while(curr!=NULL){
+            if(curr->left!=NULL){ //find pred
+                TreeNode* pred=curr->left;
+                while(pred->right!=NULL && pred->right!=curr){
+                    pred=pred->right;
+                }
+                if(pred->right==NULL){ //Link
+                    pred->right=curr;
+                    ans.push_back(curr->val);
+                    curr=curr->left;
+                }
+                if(pred->right==curr){
+                    pred->right=NULL;
                     curr=curr->right;
                 }
             }

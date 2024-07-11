e029e6e3f7bed077161e6440cd6a61cbd5c48ec5
Prince kumar <princekumar7320918928@gmail.com>
2024-07-11 18:10:15 +0530

    Convert array to heap

--- _12_Heap_or_PriorityQueues/_02_PW/_04_HeapigyAlgo.cpp
+++ _12_Heap_or_PriorityQueues/_02_PW/_04_HeapigyAlgo.cpp
@@ -1,52 +1,52 @@
-//Min Heap
-
+//min heap
 
 #include<bits/stdc++.h>
 using namespace std;
-void print(vector<int> v){
-  for(int ele:v)cout<<ele<<" ";
-  cout<<endl;
-}
 void heapify(vector<int>& v,int i){
-  int n=v.size();
-  while(true){
-    int l=2*i+1;
-    int r=2*i+2;
-    if(l<n && v[i]>v[l]){
-      if(r<n && v[r]<v[l]) {
-        swap(v[r],v[i]);
-        i=r;
-      }
-      else{
-        swap(v[i],v[l]);
-        i=l;
-      }
+    int n=v.size();
+    while(true){
+        int l=2*i+1;
+        int r=2*i+2;
+        if(l<n && v[i]>v[l]){
+            if(r<n && v[r]<v[l]){
+                swap(v[i],v[r]);
+                i=r;
+            }
+            else{
+                swap(v[i],v[l]);
+                i=l;
+            }
+        }
+        else if(r<n && v[i]>v[r]){
+            if(v[l]<v[r]){
+                swap(v[i],v[l]);
+                i=l;
+            }
+            else{
+                swap(v[i],v[r]);
+                i=r;
+            }
+        }
+        else break;
     }
-    else if(r<n && v[i]>v[r]){
-      if(v[l]<v[r]){
-        swap(v[l],v[i]);
-        i=l;
-      }
-      else{
-        swap(v[r],v[i]);
-        i=r;
-      }
-    }
-    else break;
-  }
 }
+
+void display(vector<int> v){
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+}
+
 int main(){
-  vector<int> v={10,22,14,11,1,40};
-  int n=v.size();
-  print(v);
-  // for(int i=n/2-1;i>=0;i--){
-  //   heapify(v,i);
-  // }
-  make_heap(v.begin(),v.end(),greater<int>());
-  
-  make_heap(v.begin(), v.end(),greater<int>());
-  print(v);
-  return 0;
+    vector<int> v={140,112,30,34,50,12,7410,8052,940,41};
+    int n=v.size();
+    display(v);
+    for(int i=(n-1)/2;i>=0;i--){
+        heapify(v,i);
+    }
+    display(v);
+    return 0;
 }
 
 

1e76370f7c5417e308a3999fe13a7b0efc9b066e
Prince kumar <princekumar7320918928@gmail.com>
2024-07-13 14:15:43 +0530

    786. K-th Smallest Prime Fraction

--- _12_Heap_or_PriorityQueues/_03_PW/_02_K-thSmallestPrimeFraction.cpp
+++ _12_Heap_or_PriorityQueues/_03_PW/_02_K-thSmallestPrimeFraction.cpp
@@ -14,4 +14,31 @@ public:
         sort(v.begin(),v.end());
         return {(int)v[k-1][1],(int)v[k-1][2]};
     }
-};
+}; ///O(n^2)
+
+
+
+
+
+class Solution {
+public:
+    vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
+        int n=arr.size();
+        typedef pair<double,pair<int,int>> pp;
+        priority_queue<pp,vector<pp>,greater<pp>> pq;
+        for(int i=0;i<n;i++){
+            pq.push({(double)arr[i]/arr[n-1],{i,n-1}});
+        }
+        int c=0;
+        while(pq.size()>0){
+            auto temp=pq.top();pq.pop();
+            int a=temp.second.first;
+            int b=temp.second.second;
+            c++;
+            if(c==k) return {arr[a],arr[b]};
+            b--;
+            if(a<b) pq.push({(double)arr[a]/arr[b],{a,b}});
+        }
+        return {};
+    }
+};// O(nlog(n))

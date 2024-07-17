5c5a8e03e816f414674db0f516210649e26cd5b0
Prince kumar <princekumar7320918928@gmail.com>
2024-07-17 09:31:53 +0530

    Printing Subsequences

---
+++ _13_Recursioin/_08_PrintingSubsequences.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+void printAllSubsequence(vector<int>& v,vector<int>& temp,int i,int n){
+  if(i==n) {
+    for(auto ele:temp)cout<<ele<<" ";
+    cout<<endl;
+    return;
+  }
+  temp.push_back(v[i]);
+  printAllSubsequence(v,temp,i+1,n);
+  temp.pop_back();
+  printAllSubsequence(v,temp,i+1,n);
+}
+int main(){
+  vector<int> v={1,2,3};
+  int n=v.size();
+  vector<int> temp;
+  printAllSubsequence(v,temp,0,n);
+  return 0;
+}

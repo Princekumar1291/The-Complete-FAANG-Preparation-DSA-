57e5281b2ca9c2eb678e6ccbc420f36fdf16cf30
Prince kumar <princekumar7320918928@gmail.com>
2024-07-23 12:25:06 +0530

    62. Unique Paths

---
+++ _14_DP/_03_2D/_01_gridUniquePath.cpp
@@ -0,0 +1,44 @@
+//1) express on term of index (i,j)
+//2) Explore and do all stuff
+//3) sum of all ways/min/max of all stuff
+
+class Solution {
+public:
+    int helper(int m,int n,vector<vector<int>>& dp){
+        if(m==0 && n==0) return 1;
+        else if(m<0 || n<0) return 0;
+        if(dp[m][n]!=-1) return dp[m][n];
+        int l=helper(m,n-1,dp);
+        int r=helper(m-1,n,dp);
+        return dp[m][n]=l+r;
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return helper(m-1,n-1,dp);
+    }
+};
+
+
+
+
+
+//tabulation 
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        int dp [m] [n];   
+        for (int i=0; i<m; i++){
+            for (int j=0 ;j<n ;j++){
+                if ( i==0 || j==0 ) dp[i][j]=1;
+                else{                    
+                    int l=0;
+                    if(i>0) l=dp [i-1] [j];
+                    int u=0;
+                    if(j>0) u=dp [i] [j-1];                       
+                    dp [i] [j] =l+u;
+                    }
+                }
+            }
+        return dp [ m-1 ] [ n-1 ] ;
+    }
+};

d0a608f822e6525de6e7c6a90ab7ff46718279f0
Prince kumar <princekumar7320918928@gmail.com>
2024-07-26 12:16:58 +0530

    Partition Set Into 2 Subsets With Min Absolute Sum Diff (DP- 16)

---
+++ _14_DP/_04_DPOnSubsequences/_03_PartitionSetInto2SubsetsWithMinAbsoluteSumDiff.cpp
@@ -0,0 +1,26 @@
+//Problem Link: https://leetcode.com/problems/partition-array-into-two-arrays-to-minimize-sum-difference/
+
+
+
+
+bool targetSum(vector<int>& nums,int target,int i,vector<vector<int>>& dp){
+	if(target==0) return true;
+	if(i==0) return nums[i]==target;
+	if(dp[i][target]!=-1) return dp[i][target];
+	bool notTake=targetSum(nums,target,i-1,dp);
+	bool take=false;
+	if(target>=nums[i]) take=targetSum(nums,target-nums[i],i-1,dp);
+	return dp[i][target]=take||notTake;
+}
+int minSubsetSumDifference(vector<int>& nums, int n){
+	int totSum=0;
+	for(int ele:nums) totSum+=ele;
+
+	int ans=INT_MAX;
+	vector<vector<int>> dp(n,vector<int>(totSum+1,-1));
+	for(int i=0;i<=totSum;i++){
+		bool temp=targetSum(nums,i,n-1,dp);
+		if(temp) ans=min(ans,abs(i-(totSum-i)));
+	} 
+	return ans;
+}

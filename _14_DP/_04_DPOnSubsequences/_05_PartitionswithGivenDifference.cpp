cc10e0e867a547b0a9d8ce70cdf8c26f729144f1
Prince kumar <princekumar7320918928@gmail.com>
2024-07-26 21:43:39 +0530

    Partitions with Given Difference

---
+++ _14_DP/_04_DPOnSubsequences/_05_PartitionswithGivenDifference.cpp
@@ -0,0 +1,32 @@
+prblem Link: https://www.geeksforgeeks.org/problems/partitions-with-given-difference/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=partitions-with-given-difference
+
+
+
+
+
+class Solution {
+  public:
+    int md=int(1e9+7);
+    int helper(vector<int>& arr,int target,int i,vector<vector<int>>& dp){
+        if(i==0){
+            if(target==0 && arr[0]==0) return 2;
+            else if(target==0 || target==arr[0]) return 1;
+            return 0;
+        }
+        if(dp[i][target]!=-1) return dp[i][target];
+        int notTake=helper(arr,target,i-1,dp);
+        int take=0;
+        if(target>=arr[i]) take=helper(arr,target-arr[i],i-1,dp);
+        return dp[i][target]=(take+notTake)%md;
+    }
+    int countPartitions(int n, int d, vector<int>& arr) {
+        // Code here
+        int sum=0;
+        for(int ele:arr) sum+=ele;
+        int target=sum-d;
+        if(target<0 || target%2==1) return 0;
+        target/=2;
+        vector<vector<int>> dp(n,vector<int>(target+1,-1));
+        return helper(arr,target,n-1,dp);
+    }
+};

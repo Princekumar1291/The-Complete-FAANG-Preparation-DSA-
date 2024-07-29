3db7afaaf6788baab453dd1fdae965d377022cd6
Prince kumar <princekumar7320918928@gmail.com>
2024-07-29 21:42:50 +0530

    122. Best Time to Buy and Sell Stock II

---
+++ _14_DP/_06_DPonStock/_02_BestTimeToBuyAndSellStockII.cpp
@@ -0,0 +1,20 @@
+//Problem link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+
+
+
+
+
+class Solution {
+public:
+    int helper(vector<int>& arr,int i,int check,vector<vector<int>>& dp){
+        if(i==arr.size()) return 0;
+        if(dp[i][check]!=INT_MIN) return dp[i][check];
+        if(check==1) return dp[i][check]=max((-arr[i]+helper(arr,i+1,0,dp)),helper(arr,i+1,1,dp));
+        else return dp[i][check]=max((arr[i]+helper(arr,i+1,1,dp)),helper(arr,i+1,0,dp));
+    }
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        vector<vector<int>> dp(n,vector<int>(2,INT_MIN));
+        return helper(prices,0,1,dp);
+    }
+};

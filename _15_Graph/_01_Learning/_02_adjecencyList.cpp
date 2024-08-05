5d150605af364e72260a99dedca910308bd9f917
Prince kumar <princekumar7320918928@gmail.com>
2024-08-05 09:20:00 +0530

    adjacency list

--- _15_Graph/_01_Learning/_02_adjecencyList.cpp
+++ _15_Graph/_01_Learning/_02_adjecencyList.cpp
@@ -1,35 +1,84 @@
-//adjacency list --> in this approch we represent a graph as a array of link list
+// //adjacency list --> in this approch we represent a graph as a array of list
 
+
+
+
+
+
+
+//undirected graph
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//   int n,m;
+//   cin>>n>>m;
+//   vector<vector<int>> graph(n,vector<int>());
+//   for(int i=0;i<m;i++){
+//     int s,d;
+//     cin>>s>>d;
+//     graph[s].push_back(d);
+//     graph[d].push_back(s);
+//   }
+
+//   for(int i=0;i<n;i++){
+//     cout<<i<<"---->";
+//     for(int ele:graph[i]){
+//       cout<<ele<<" ";
+//     }
+//     cout<<endl;
+//   }
+//   return 0;
+// }   //O(2E)
+
+
+
+
+// //directed graph
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//   int n,m;
+//   cin>>n>>m;
+//   vector<vector<int>> graph(n,vector<int>());
+//   for(int i=0;i<m;i++){
+//     int s,d;
+//     cin>>s>>d;
+//     graph[s].push_back(d);
+//   }
+
+//   for(int i=0;i<n;i++){
+//     cout<<i<<"---->";
+//     for(int ele:graph[i]){
+//       cout<<ele<<" ";
+//     }
+//     cout<<endl;
+//   }
+//   return 0;
+// }   //O(E)
+
+
+
+
+//weighted graph
 #include<bits/stdc++.h>
 using namespace std;
-
-vector<vector<int>> graph;
-int v;
-void add_edge(int src,int dest,bool bidir=true){
-  graph[src].push_back(dest);
-  if(bidir) graph[dest].push_back(src);
-}
-
-void display(){
-  for(int i=0;i<graph.size();i++){
-    cout<<i<<" -> ";
-    for(auto ele:graph[i]){
-      cout<<ele<<" , ";
-    }cout<<endl;
-  }
-}
-
 int main(){
-  cin>>v;
-  graph.resize(v,vector<int>());
-
-  int e;
-  cin>>e;
-  while(e--){
-    int s,d;
-    cin>>s>>d;
-    add_edge(s,d);
+  int n,m;
+  cin>>n>>m;
+  vector<vector<pair<int,int>>> graph(n,vector<pair<int,int>>());
+  for(int i=0;i<m;i++){
+    int s,d,w;
+    cin>>s>>d>>w;
+    graph[s].push_back({d,w});
+    graph[d].push_back({s,w});
   }
-  display();
+
+  for(int i=0;i<n;i++){
+    cout<<i<<"---->";
+    for(auto ele:graph[i]){
+      cout<<"{"<<ele.first<<","<<ele.second<<"} ";
+    }
+    cout<<endl;
+  }
   return 0;
-}
+}   

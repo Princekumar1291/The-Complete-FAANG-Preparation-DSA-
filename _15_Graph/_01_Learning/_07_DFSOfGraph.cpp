20639314500be61b4304378e978c2ff21c8b8118
Prince kumar <princekumar7320918928@gmail.com>
2024-08-05 14:55:45 +0530

    DFS of Graph

---
+++ _15_Graph/_01_Learning/_07_DFSOfGraph.cpp
@@ -0,0 +1,27 @@
+//Problem Link: https://www.geeksforgeeks.org/problems/depth-first-traversal-for-a-graph/1
+
+
+
+
+
+class Solution {
+  public:
+    void dfsFun(int node,vector<int>& dfs,vector<int>& vis,vector<int> adj[]){
+        vis[node]=1;
+        dfs.push_back(node);
+        for(auto ele:adj[node]){
+            if(vis[ele]==0){
+                vis[ele]=1;
+                dfsFun(ele,dfs,vis,adj);
+            }
+        }
+    }
+    vector<int> dfsOfGraph(int v, vector<int> adj[]) {
+        // Code here
+        int start=0;
+        vector<int> dfs;
+        vector<int> vis(v,0);
+        dfsFun(start,dfs,vis,adj);
+        return dfs;
+    }
+};

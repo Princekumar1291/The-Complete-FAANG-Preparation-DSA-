e6ac0c79532f9e6a98eacecf7850793b41abb96d
Prince kumar <princekumar7320918928@gmail.com>
2024-08-10 12:19:43 +0530

    210. Course Schedule II

---
+++ _15_Graph/_03_TopoSortProblems/_05_CourseSchedule2.cpp
@@ -0,0 +1,37 @@
+//Problem Link: https://leetcode.com/problems/course-schedule-ii/description/
+
+
+
+
+class Solution {
+public:
+    vector<int> findOrder(int v, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(v,vector<int>());
+        for(auto arr:prerequisites){
+            adj[arr[1]].push_back(arr[0]);
+        }
+        vector<int> indeg(v,0);
+        for(auto arr:adj){
+            for(int ele:arr){
+                indeg[ele]++;
+            }
+        }
+
+        queue<int> q;
+        for(int i=0;i<v;i++){
+            if(indeg[i]==0) q.push(i);
+        }
+
+        vector<int> ans;
+        while(!q.empty()){
+            int curr=q.front();
+            q.pop();
+            ans.push_back(curr);
+            for(int ele:adj[curr]){
+                indeg[ele]--;
+                if(indeg[ele]==0) q.push(ele);
+            }
+        }
+        return ans.size()==v ? ans : vector<int>(0);
+    }
+};

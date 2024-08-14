e86c0ddd33c2e0409076630955b56d45e2732517
Prince kumar <princekumar7320918928@gmail.com>
2024-08-14 09:15:53 +0530

    longestCommonSubsequence

--- _14_DP/_05_DPonString/_01_LongestCommonSubsequence.cpp
+++ _14_DP/_05_DPonString/_01_LongestCommonSubsequence.cpp
@@ -18,3 +18,27 @@ public:
         return helper(text1,text2,n1-1,n2-1,dp);
     }
 };
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    int longestCommonSubsequence(string s1, string s2) {
+        int n=s1.size();
+        int m=s2.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1[i-1]==s2[j-1]) dp[i][j]=1+dp[i-1][j-1];
+                else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[n][m];
+    }
+};

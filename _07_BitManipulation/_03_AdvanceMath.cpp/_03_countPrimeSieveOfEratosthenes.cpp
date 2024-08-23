bdf61616013187ed8ee962978e7840ca17709ce0
Prince kumar <princekumar7320918928@gmail.com>
2024-08-23 20:27:54 +0530

    Sieve of Eratosthenes     204. Count Primes

--- _07_BitManipulation/_03_AdvanceMath.cpp/_03_countPrimeSieveOfEratosthenes.cpp
+++ _07_BitManipulation/_03_AdvanceMath.cpp/_03_countPrimeSieveOfEratosthenes.cpp
@@ -24,7 +24,21 @@ int countPrimes(int n) {
 }
 
 
-int main(){
-  
-  return 0;
-}
+class Solution {
+public:
+    int countPrimes(int n) {
+        vector<bool> v(n,1);
+        for(int i=2;i<sqrt(n);i++){
+            if(v[i]==1){
+                for(int j=i*i;j<n;j+=i){
+                    v[j]=0;
+                }
+            }
+        }
+        int ans=0;
+        for(int i=2;i<n;i++){
+            if(v[i]==1) ans++;
+        }
+        return ans;
+    }
+};

05974d56ad9e5d99376c0ae650ef6dc4eedd2f46
Prince kumar <princekumar7320918928@gmail.com>
2024-08-23 20:48:41 +0530

    Sieve of Eratosthenes     204. Count Primes

--- _07_BitManipulation/_03_AdvanceMath.cpp/_03_countPrimeSieveOfEratosthenes.cpp
+++ _07_BitManipulation/_03_AdvanceMath.cpp/_03_countPrimeSieveOfEratosthenes.cpp
@@ -28,7 +28,7 @@ class Solution {
 public:
     int countPrimes(int n) {
         vector<bool> v(n,1);
-        for(int i=2;i<sqrt(n);i++){
+        for(int i=2;i*i<n;i++){
             if(v[i]==1){
                 for(int j=i*i;j<n;j+=i){
                     v[j]=0;

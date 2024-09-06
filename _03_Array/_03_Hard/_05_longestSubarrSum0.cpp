57718542eb767359fd53acd142ced455ef332bab
Prince kumar <princekumar7320918928@gmail.com>
2024-09-06 07:52:56 +0530

    longest subarray sum

--- _03_Array/_03_Hard/_05_longestSubarrSum0.cpp
+++ _03_Array/_03_Hard/_05_longestSubarrSum0.cpp
@@ -1,25 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int maxLen(vector<int>&v, int n){   
-  unordered_map<int,int> mp;
-  int sum=0;
-  int l,r;
-  int ans=0;
-  for(int i=0;i<n;i++){
-      sum+=v[i];
-      int rem=sum;
-      if(sum==0) ans=i+1;
-      else if(mp.find(rem)!=mp.end()){
-          l=mp[rem];
-          r=i;
-          ans=max(ans,r-l);
-      }
-      if(mp.find(sum)==mp.end()){
-          mp[sum]=i;
-      }
-  }
-  return ans;
+int maxLen(vector<int>& arr, int n) {
+    unordered_map<int,int> mp;
+    int ans=0;
+    int sum=0;
+    for(int i=0;i<n;i++){
+        sum+=arr[i];
+        if(sum==0) ans=i+1;
+        if(mp.find(sum)!=mp.end()){
+            ans=max(ans,(i-mp[sum]));
+        }
+        if(mp.find(sum)==mp.end()) mp[sum]=i;
+    }
+    return ans;
 }
 
 

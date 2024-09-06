fdf002dc3a593de2f35cf4600988da6659d8b6b3
Prince kumar <princekumar7320918928@gmail.com>
2024-09-06 07:52:16 +0530

    Subarrays with XOR ‘K’

--- _03_Array/_03_Hard/_06_SubarrayWithGivenXOR.cpp
+++ _03_Array/_03_Hard/_06_SubarrayWithGivenXOR.cpp
@@ -50,6 +50,23 @@ int subarraysWithXorKOpt(vector<int> v,int k){
   return count;
 }
 
+int subarraysWithSumK(vector < int > v, int b) {
+  int n=v.size();
+  int xr=0;
+  unordered_map<int,int> mp;
+  int ans=0;
+  for(int i=0;i<n;i++){
+    xr^=v[i];
+    int temp=xr^b;
+    if(xr==b) ans++;
+    if(mp.find(temp)!=mp.end()){
+      ans+=mp[temp];
+    }
+    mp[xr]++;
+  }
+  return ans;
+}
+
 int main(){
     vector<int> a = {4, 2, 2, 6, 4};
     int k = 6;

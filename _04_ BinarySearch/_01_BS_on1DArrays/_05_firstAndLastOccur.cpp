69d71131b01207b32ddb9647229376ad62b50ed2
Prince kumar <princekumar7320918928@gmail.com>
2024-09-09 09:15:58 +0530

    34. Find First and Last Position of Element in Sorted Array

--- _04_ BinarySearch/_01_BS_on1DArrays/_05_firstAndLastOccur.cpp
+++ _04_ BinarySearch/_01_BS_on1DArrays/_05_firstAndLastOccur.cpp
@@ -1,3 +1,6 @@
+//Problem Linl: https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -13,33 +16,42 @@ int solve(int n, int key, vector < int > & v) {
 }
 
 vector<int> searchRange(vector<int>& nums, int target) {
-        int n=nums.size();
-        int first=-1;
-        int last=-1;
-        int low=0;
-        int high=n-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(nums[mid]==target) {
-                first=mid;
-                high=mid-1;
-            }
-            else if(nums[mid]>target) high=mid-1;
-            else if(nums[mid]<target) low=mid+1;
-        }
-        low=0;
-        high=n-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(nums[mid]==target) {
-                last=mid;
-                low=mid+1;
-            }
-            else if(nums[mid]>target) high=mid-1;
-            else if(nums[mid]<target) low=mid+1;
-        }
-        return {first,last};
-    }
+  int n=nums.size();
+  int first=-1;
+  int last=-1;
+  int low=0;
+  int high=n-1;
+  while(low<=high){
+      int mid=low+(high-low)/2;
+      if(nums[mid]==target) {
+          first=mid;
+          high=mid-1;
+      }
+      else if(nums[mid]>target) high=mid-1;
+      else if(nums[mid]<target) low=mid+1;
+  }
+  low=0;
+  high=n-1;
+  while(low<=high){
+      int mid=low+(high-low)/2;
+      if(nums[mid]==target) {
+          last=mid;
+          low=mid+1;
+      }
+      else if(nums[mid]>target) high=mid-1;
+      else if(nums[mid]<target) low=mid+1;
+  }
+  return {first,last};
+}
+
+
+vector<int> searchRange(vector<int>& nums, int target) {
+  int fl=lower_bound(nums.begin(),nums.end(),target)-nums.begin();
+  int cl=upper_bound(nums.begin(),nums.end(),target)-nums.begin();
+  if(fl==cl) return {-1,-1};
+  return {fl,cl-1};
+}
+
 
 int main() {
   int n = 7;

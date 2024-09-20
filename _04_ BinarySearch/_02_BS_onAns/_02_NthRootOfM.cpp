3af98b7ead4c03ba23a7c3d18d055ab4eb73b03e
Prince <princekumar7320918928@gmail.com>
2024-09-20 07:51:53 +0530

    Find Nth root of M

--- _04_ BinarySearch/_02_BS_onAns/_02_NthRootOfM.cpp
+++ _04_ BinarySearch/_02_BS_onAns/_02_NthRootOfM.cpp
@@ -1,3 +1,6 @@
+//Problem Link: https://www.geeksforgeeks.org/problems/find-nth-root-of-m5843/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=find-nth-root-of-m
+
+
 #include<bits/stdc++.h>
 using namespace std;
 

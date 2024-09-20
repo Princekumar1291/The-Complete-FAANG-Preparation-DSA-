3e3bbb58aa7b5128ac008a4705d28851deeadd4b
Prince <princekumar7320918928@gmail.com>
2024-09-20 09:44:31 +0530

    1482. Minimum Number of Days to Make m Bouquets

--- _04_ BinarySearch/_02_BS_onAns/_04_flowerProblem.cpp
+++ _04_ BinarySearch/_02_BS_onAns/_04_flowerProblem.cpp
@@ -1,41 +1,37 @@
+//Problem Link: https://leetcode.com/problems/minimum-number-of-days-to-make-m-bouquets/description/
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
-int helper(vector<int> v,int mid,int k){
-  int n=v.size();
-  int ans=0;
-  int t=0;
-  for(int i=0;i<n;i++){
-      if(v[i]<=mid){
-          t++;
-      }
-      else{
-          ans+=t/k;
-          t=0;
-      }
-  }
-  ans+=t/k;
-  return ans;
-}
-int minDays(vector<int>& v, int m, int k) {
-  if((long long)m*k>v.size()) return -1;
-  int maxi=*max_element(v.begin(),v.end());
-  int low=1;
-  int high=maxi;
-  int ans=-1;
-  while(low<=high){
-      int mid=low+(high-low)/2;
-      int totalBuketForm=helper(v,mid,k);
-      if(totalBuketForm<m){
-          low=mid+1;
-      }
-      else{
-          ans=mid;
-          high=mid-1;
-      }
-
-  }
-  return ans;
+int minDays(vector<int>& bloomDay, int m, int k) {
+    int n=bloomDay.size();
+    int low=1;
+    int high=*max_element(bloomDay.begin(),bloomDay.end());
+    int ans=-1;
+    while(low<=high){
+        int mid=low+(high-low)/2;
+        int bouquet=0;
+        int temp=0;
+        for(int i=0;i<n;i++){
+            if(bloomDay[i]<=mid) temp++;
+            else {
+                temp=0;
+            }
+            if(temp==k){
+                bouquet++;
+                temp=0;
+            }
+        }
+        if(bouquet<m){
+            low=mid+1;
+        }
+        else{
+            ans=mid;
+            high=mid-1;
+        }
+    }
+    return ans;
 }
 
 int main() {

d406f0d73219c418be8d2626ff61a21d1c161a3f
Prince <princekumar7320918928@gmail.com>
2024-09-20 07:26:59 +0530

    Square root of a number

--- _04_ BinarySearch/_02_BS_onAns/_01_findSqrt.cpp
+++ _04_ BinarySearch/_02_BS_onAns/_01_findSqrt.cpp
@@ -1,24 +1,26 @@
+//Problem Link:https://www.geeksforgeeks.org/problems/square-root/0?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=square-root
+
 #include<bits/stdc++.h>
 using namespace std;
 
-int sqrtRoot(int x){
-  long long low=1;
-  long long high=x;
-  long long ans=1;
+long long int floorSqrt(long long int n) {
+  long long int temp;
+  long long int low=1;
+  long long int high=n;
+  long long int ans;
   while(low<=high){
-    long long mid=low+(high-low)/2;
-    long long temp=mid*mid;
-    if(temp<=(long long)x) {
-        ans=mid;
-        low=mid+1;
-    }
-    else high=mid-1;
+      long long int mid=low+(high-low)/2;
+      if(mid*mid<=n){
+          ans=mid;
+          low=mid+1;
+      }
+      else high=mid-1;
   }
   return ans;
 }
 
 int main(){
-  int ans=sqrtRoot(24);
+  int ans=floorSqrt(24);
   cout<<ans;
   return 0;
 }

6344c60cd85b5cfdea072db87b851af06deeb91c
Prince <princekumar7320918928@gmail.com>
2024-09-23 08:13:11 +0530

    74. Search a 2D Matrix

--- _04_ BinarySearch/_03_BSon2DArrays/_02_targetInSortedArray.cpp
+++ _04_ BinarySearch/_03_BSon2DArrays/_02_targetInSortedArray.cpp
@@ -1,3 +1,5 @@
+//Problem Link: https://leetcode.com/problems/search-a-2d-matrix/description/
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -27,22 +29,21 @@ bool searchMatrix(vector<vector<int>>& v, int target) {
 }
 
 bool searchMatrix(vector<vector<int>>& matrix, int target) {
-  int n = matrix.size();
-  int m = matrix[0].size();
-
-  //apply binary search:
-  int low = 0, high = n * m - 1;
-  while (low <= high) {
-    int mid = (low + high) / 2;
-    int row = mid / m, col = mid % m;
-    if (matrix[row][col] == target) return true;
-    else if (matrix[row][col] < target) low = mid + 1;
-    else high = mid - 1;
+  int n=matrix.size();
+  int m=matrix[0].size();
+  int i=0;
+  int j=m-1;
+  while(i<n && j>=0){
+      if(matrix[i][j]==target) return true;
+      else if(target<matrix[i][j]) j--;
+      else i++;
   }
   return false;
 }
 
 int main(){
-  
+  vector<vector<int>> v = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
+  int target=3;
+  cout<<searchMatrix(v,target);
   return 0;
 }

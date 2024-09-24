24d34e154c7d3f182b5d7b9b939e9b5d8c6c369b
Prince <princekumar7320918928@gmail.com>
2024-09-24 08:47:22 +0530

    Strings Rotations of Each Other

--- _05_String/_100_Negi/_02_CheckIfStringsAreRotationsofEachOtherOrNot.cpp
+++ _05_String/_100_Negi/_02_CheckIfStringsAreRotationsofEachOtherOrNot.cpp
@@ -1,4 +1,4 @@
-Problem Link: https://www.geeksforgeeks.org/problems/check-if-strings-are-rotations-of-each-other-or-not-1587115620/1?page=1&difficulty%255B%255D=-1&category%255B%255D=Strings&sortBy=submissions
+// Problem Link: https://www.geeksforgeeks.org/problems/check-if-strings-are-rotations-of-each-other-or-not-1587115620/1?page=1&difficulty%255B%255D=-1&category%255B%255D=Strings&sortBy=submissions
 
 
 #include<bits/stdc++.h>

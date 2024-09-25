80bc6aa14e8093173ae136622760016356d811d8
Prince <princekumar7320918928@gmail.com>
2024-09-25 10:23:14 +0530

    Smallest window containing 0, 1 and 2

--- _05_String/_100_Negi/_16_smallestWindowContaning012.cpp
+++ _05_String/_100_Negi/_16_smallestWindowContaning012.cpp
@@ -1,34 +1,27 @@
+//Problem Link: https://www.geeksforgeeks.org/problems/smallest-window-containing-0-1-and-2--170637/1
+
 #include<bits/stdc++.h>
 using namespace std;
 
-int smallestSubstring(string s) {
-  // Code here
-  int n=s.size();
-  int arr[3]={0};
-  for(int i=0;i<n;i++){
-    if(s[i]=='0') arr[0]++;
-    else if(s[i]=='1') arr[1]++;
-    else if(s[i]=='2') arr[2]++;
-  }
-  
-  for(int i=0;i<3;i++){
-    if(arr[i]==0) return -1;
-  }
-  
-  int ans=n;
+
+int smallestSubstring(string S) {
+  int n=S.size();
+  int ans=INT_MAX;
+  int low=0;
   unordered_map<char,int> mp;
   for(int i=0;i<n;i++){
-    mp[s[i]]=i;
-    if(mp.size()==3){
-      int a=mp['1'];
-      int b=mp['2'];
-      int c=mp['0'];
-      int maxi=max(a,max(b,c));
-      int mini=min(a,min(b,c));
-      ans=min(ans,maxi-mini+1);
-    }
+      mp[S[i]]++;
+      if(mp.find(S[low])!=mp.end() && mp[S[low]]>1){
+          while(mp[S[low]]>1) {
+              mp[S[low]]--;
+              low++;
+          }
+      }
+      if(mp.size()==3){
+          ans=min(ans,i-low+1);
+      }
   }
-  return ans;
+  return ans==INT_MAX ? -1 : ans;
 }
 
 int main(){

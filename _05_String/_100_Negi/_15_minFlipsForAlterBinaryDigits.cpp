9970d964278f3d4e2563529f7e96dc516140a8bc
Prince <princekumar7320918928@gmail.com>
2024-09-25 08:46:07 +0530

    Min Number of Flips

--- _05_String/_100_Negi/_15_minFlipsForAlterBinaryDigits.cpp
+++ _05_String/_100_Negi/_15_minFlipsForAlterBinaryDigits.cpp
@@ -1,3 +1,5 @@
+//Problem Link: https://www.geeksforgeeks.org/problems/min-number-of-flips3210/1?page=2&difficulty%255B%255D=0&category%255B%255D=Strings&sortBy=submissions
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -18,6 +20,36 @@ int minFlips (string s){
   }
   return  min(ans1,ans2);
 }
+
+
+int minFlips (string S){
+    int n=S.size();
+    int ans1=0;
+    int ans2=0;
+    int c1=0;
+    int c2=0;
+    for(int i=0;i<n;i++){
+        if(i%2==0){
+            if(S[i]=='0') c1++;
+        }
+        else{
+            if(S[i]=='1') c2++;
+        }
+    }
+    ans1=c1+c2;
+    c1=0,c2=0;
+    for(int i=0;i<n;i++){
+        if(i%2==0){
+            if(S[i]=='1') c1++;
+        }
+        else{
+            if(S[i]=='0') c2++;
+        }
+    }
+    ans2=c1+c2;
+    return min(ans1,ans2);
+}
+
 
 int main(){
   

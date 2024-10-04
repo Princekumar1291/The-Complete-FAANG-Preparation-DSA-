9e2ffc549b5a00ff1899f7a577daf8b3e2e1135c
Prince <princekumar7320918928@gmail.com>
2024-10-04 07:21:49 +0530

    1ToNto1

---
+++ _06.5_recursion/_01_basic/_03_1ToNTo1.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+void f(int n){
+  if(n==0) return;
+  f(n-1);
+  cout<<n<<" ";
+}
+void f2(int n){
+  if(n==0) return ;
+  cout<<n<<" ";
+  f2(n-1);
+}
+int main(){
+  int n=12;
+  f(n);cout<<endl;
+  f2(n);
+  return 0;
+}

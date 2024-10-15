7fabdc50ddde79d5f388857e6fd6583d0a93e0eb
Prince <princekumar7320918928@gmail.com>
2024-10-15 09:54:49 +0530

    Infix to postfix

--- demo.cpp
+++ demo.cpp
@@ -1,35 +1,7 @@
-#include <iostream>
-#include <vector>
-
+// Online C++ compiler to run C++ program online
+#include <bits/stdc++.h>
 using namespace std;
-
-vector<vector<int>> findSubsets(const vector<int>& nums) {
-    int n = nums.size();
-    int totalSubsets = 1 << n;
-    vector<vector<int>> subsets;
-
-    for (int i = 0; i < totalSubsets; i++) {
-        vector<int> currentSubset;
-        for (int j = 0; j < n; j++) {
-            if (i & (1 << j)) {
-                currentSubset.push_back(nums[j]);
-            }
-        }
-        subsets.push_back(currentSubset);
-    }
-
-    return subsets;
-}
-
 int main() {
-    vector<int> nums = {1, 2, 3};
-    vector<vector<int>> subsets = findSubsets(nums);
-
-    for (const auto& subset : subsets) {
-        for (int num : subset) {
-            cout << num << " ";
-        }cout << endl;
-    }
-
+    cout<<isalpha('/');
     return 0;
 }

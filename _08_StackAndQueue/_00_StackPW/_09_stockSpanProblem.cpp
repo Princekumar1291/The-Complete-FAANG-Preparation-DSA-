f5cc6271d1d8f40c2518e18ba9c4274f81bdc484
Prince <princekumar7320918928@gmail.com>
2024-10-15 07:42:10 +0530

    Stock span problem

--- _08_StackAndQueue/_00_StackPW/_09_stockSpanProblem.cpp
+++ _08_StackAndQueue/_00_StackPW/_09_stockSpanProblem.cpp
@@ -4,26 +4,18 @@
 using namespace std;
 
 vector <int> calculateSpan(int price[], int n){
-  vector<int> ans(n);
-  stack<int> st;
-  ans[0]=-1;
-  st.push(0);
-  for(int i=1;i<n;i++){
-    while(!st.empty() && price[st.top()]<=price[i]){
-        st.pop();
+    // previous greater element
+    stack<int> st;
+    vector<int> ans(n);
+    for(int i=0;i<n;i++){
+        while(!st.empty() && price[st.top()]<=price[i]){
+            st.pop();
+        }
+        if(st.empty()) ans[i]=i+1;
+        else ans[i]=i-st.top();
+        st.push(i);
     }
-    if(st.empty()){
-        ans[i]=-1;
-    }
-    else{
-        ans[i]=st.top();
-    }
-    st.push(i);
-  }
-  for(int i=0;i<n;i++){
-      ans[i]=i-ans[i];
-  }
-  return ans;
+    return ans;
 }
 
 int main(){

5c88f580ca18ba2919bb4067bf3a1c8c13fc8b09
Prince <princekumar7320918928@gmail.com>
2024-10-22 11:33:07 +0530

    1052. Grumpy Bookstore Owner

--- _09_SlidingWindow&TwoPointer/_01_PW_SlidingWindow/_02_GrumpyBookstoreOwner.cpp
+++ _09_SlidingWindow&TwoPointer/_01_PW_SlidingWindow/_02_GrumpyBookstoreOwner.cpp
@@ -1,39 +1,43 @@
 // Problem Link: https://leetcode.com/problems/grumpy-bookstore-owner/description/
 
-#include<bits/stdc++.h>
-using namespace std;
+class Solution {
+public:
+    int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
+        int n=grumpy.size();
+        int temp1=0;
+        int max1Ind=0;
+        int notSat=0;
+        int tempNotSat=0;
+        for(int i=0;i<minutes;i++) {
+            if(grumpy[i]==1) {
+                temp1++;
+                tempNotSat+=customers[i];
+            }
+        }
+        notSat=tempNotSat;
 
-int maxSatisfied(vector<int>& c, vector<int>& g, int m) {
-  int n=c.size();
-  int maxLoss=0;
-  int i=0;
-  for(i=0;i<m;i++){
-      if(g[i]==1) maxLoss+=c[i];
-  }
-
-  int indx=0;
-  int t=0;
-  int maxAns=maxLoss;
-  for(int j=i;j<n;j++){
-      if(g[t]==1) maxLoss-=c[t];
-      t++;
-      if(g[j]==1) maxLoss+=c[j];
-      if(maxLoss>maxAns){
-          maxAns=maxLoss;
-          indx=j;
-      }
-  }
-
-  int ansFinal=0;
-  for(int i=0;i<n;i++) {
-      if(g[i]==0) ansFinal+= c[i];
-  }
-  ansFinal+=maxAns;
-  return ansFinal;
-}
-
-
-int main(){
-  
-  return 0;
-}
+        for(int i=minutes;i<n;i++){
+            temp1=temp1+grumpy[i]-grumpy[i-minutes];
+            if(grumpy[i]==1){
+                tempNotSat+=customers[i];
+            }
+            if(grumpy[i-minutes]==1){
+                tempNotSat-=customers[i-minutes];
+            }
+            if(tempNotSat>notSat && temp1>0){
+                max1Ind=i-minutes+1;
+                notSat=tempNotSat;
+            }
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(i>=max1Ind && i<max1Ind+minutes){
+                ans+=customers[i];
+            }
+            else{
+                if(grumpy[i]==0) ans+=customers[i];
+            }
+        }
+        return ans;
+    }
+};

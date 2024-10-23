a730f0686aa6ef97a3ac371403217b7e2d24975d
Prince <princekumar7320918928@gmail.com>
2024-10-23 11:03:03 +0530

    first neg int the window

--- _09_SlidingWindow&TwoPointer/_01_PW_SlidingWindow/_03_FirstNegNumberInKWindow.cpp
+++ _09_SlidingWindow&TwoPointer/_01_PW_SlidingWindow/_03_FirstNegNumberInKWindow.cpp
@@ -3,6 +3,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//brute force
 vector<long long> printFirstNegativeInteger(long long int arr[],long long int n, long long int k){
   vector<long long > ans;
   for(int i=0;i<n-k+1;i++){
@@ -19,9 +20,32 @@ vector<long long> printFirstNegativeInteger(long long int arr[],long long int n,
   return ans;
 }
 
+//sliding window
+vector<int> printFirstNegativeInteger2(vector<int> v,int N, int K) {
+    queue<int> q;
+    vector<int> ans;
+    for(int i=0;i<N;i++) if(v[i]<0) q.push(i);
+    for(int i=K-1;i<N;i++){
+        if(q.empty()){
+            ans.push_back(0);
+        }
+        else if(q.front()>=i-K+1 && q.front()<=i){
+            ans.push_back(v[q.front()]);
+        }
+        else ans.push_back(0);
+        if(!q.empty() && q.front()==i-K+1) q.pop();
+    }
+    return ans;
+ }
+
+
+
 
 int main(){
-  
+  int n=9,k=4;
+  vector<int> v={2,-3,4,4,-7,-1,4,-2,6};
+  vector<int> ans=printFirstNegativeInteger2(v,n,k);
+  for(int i=0;i<ans.size();i++) cout<<ans[i]<<" "; 
   return 0;
 }
 

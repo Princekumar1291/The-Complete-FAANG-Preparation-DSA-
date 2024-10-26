4fdc0bba99d97f40a8dc03c585b9882ab86ddfe5
Prince <princekumar7320918928@gmail.com>
2024-10-26 12:52:32 +0530

    16. 3Sum Closest

--- _09_SlidingWindow&TwoPointer/_01_PW_SlidingWindow/_08_3SumClosest.cpp
+++ _09_SlidingWindow&TwoPointer/_01_PW_SlidingWindow/_08_3SumClosest.cpp
@@ -1,28 +1,28 @@
 //Problem Link: https://leetcode.com/problems/3sum-closest/
 
 
-
-class Solution {
-public:
-    int threeSumClosest(vector<int>& arr, int target) {
-        sort(arr.begin(),arr.end());
-        int gap=INT_MAX;
-        int ans;
-        int n=arr.size();
-        for(int i=0;i<n-2;i++){
-            int j=i+1;
-            int k=n-1;
-            while(j<k){
-                int sum=arr[i]+arr[j]+arr[k];
-                int currGap=abs(sum-target);
-                if(currGap<gap){
-                    ans=sum;
-                    gap=currGap;
-                }
-                if(sum<target) j++;
-                else k--;
-            }
+#include <bits/stdc++.h>
+using namespace std;
+int threeSumClosest(vector<int>& arr, int target) {
+    sort(arr.begin(), arr.end());
+    int n = arr.size();
+    int ans = arr[0] + arr[1] + arr[2];
+    for (int i = 0; i < n - 2; i++) {
+        int j = i + 1;
+        int k = n - 1;
+        while (j < k) {
+            int sum = arr[i] + arr[j] + arr[k];
+            if (abs(sum - target) < abs(target - ans)) ans = sum;
+            if (sum < target) j++;
+            else k--; 
         }
-        return ans;
     }
-};
+    return ans;
+}
+
+int main(){
+    vector<int> arr = {-1, 2, 1, -4};
+    int target = 1;
+    cout<<threeSumClosest(arr, target);
+    return;
+}

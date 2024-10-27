490b5fb92765c6c93b677f2fd4d7fdb70ca72fa4
Prince <princekumar7320918928@gmail.com>
2024-10-27 08:08:56 +0530

    215. Kth Largest Element in an Array

--- demo.cpp
+++ demo.cpp
@@ -1,16 +1,20 @@
+//Problem Link: https://leetcode.com/problems/kth-largest-element-in-an-array/submissions/1434771479/
+
+
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-  set<int> st;
-  st.insert(1);
-  st.insert(3);
-  st.insert(3);
-  st.insert(2);
-  st.insert(4);
-
-  st.erase(3);
-  for(auto ele:st){
-    cout<<ele<<" ";
+  vector<int> v={10,20,-4,6,18,24,105,118};
+  int n=v.size();
+  int k=8;
+  priority_queue<int> pq;
+  for(int i=0;i<n;i++){
+    pq.push(v[i]);
+    if(pq.size()>k) pq.pop();
   }
+  cout<<pq.top()<<endl;
   return 0;
 }
+
+// time--> O(nlog(k))
+// space--> O(k)

799d0721b76d41f807316c6b105e8dddd54ff895
Prince <princekumar7320918928@gmail.com>
2024-10-27 18:24:20 +0530

    heapify algo

--- _12_Heap_or_PriorityQueues/_02_PW/_04_HeapifyAlgo.cpp
+++ _12_Heap_or_PriorityQueues/_02_PW/_04_HeapifyAlgo.cpp
@@ -4,28 +4,16 @@
 using namespace std;
 void heapify(vector<int>& v,int i){
     int n=v.size();
-    while(true){
+    while(i*2+1<n){
         int l=2*i+1;
         int r=2*i+2;
-        if(l<n && v[i]>v[l]){
-            if(r<n && v[r]<v[l]){
-                swap(v[i],v[r]);
-                i=r;
-            }
-            else{
-                swap(v[i],v[l]);
-                i=l;
-            }
+        if(r<n && v[r]<v[l] && v[r]<v[i]){
+          swap(v[r],v[i]);
+          i=r;
         }
-        else if(r<n && v[i]>v[r]){
-            if(v[l]<v[r]){
-                swap(v[i],v[l]);
-                i=l;
-            }
-            else{
-                swap(v[i],v[r]);
-                i=r;
-            }
+        else if(l<n && v[l]<v[i]){
+          swap(v[l],v[i]);
+          i=l;
         }
         else break;
     }
@@ -39,10 +27,10 @@ void display(vector<int> v){
 }
 
 int main(){
-    vector<int> v={140,112,30,34,50,12,7410,8052,940,41};
+    vector<int> v={140,112,30,34,50,12,7410,8052,940,41}; //12 34 30 112 41 140 7410 8052 940 50 
     int n=v.size();
     display(v);
-    for(int i=(n-1)/2;i>=0;i--){
+    for(int i=n/2;i>=0;i--){
         heapify(v,i);
     }
     display(v);

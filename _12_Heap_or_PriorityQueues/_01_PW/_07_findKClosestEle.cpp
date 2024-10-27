93bc431da1a22dbdc1c043858256c376206c9ce7
Prince <princekumar7320918928@gmail.com>
2024-10-27 12:16:15 +0530

    658. Find K Closest Elements

--- _12_Heap_or_PriorityQueues/_01_PW/_07_findKClosestEle.cpp
+++ _12_Heap_or_PriorityQueues/_01_PW/_07_findKClosestEle.cpp
@@ -20,3 +20,20 @@ public:
         return ans;
     }
 };
+
+
+vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+    int n=arr.size();
+    priority_queue<pair<int,int>> pq;
+    for(int i=0;i<n;i++){
+        pq.push({abs(x-arr[i]),arr[i]});
+        if(pq.size()>k) pq.pop();
+    }
+    vector<int> ans;
+    while(pq.size()>0) {
+        ans.push_back(pq.top().second);
+        pq.pop();
+    }
+    sort(ans.begin(),ans.end());
+    return ans;
+}

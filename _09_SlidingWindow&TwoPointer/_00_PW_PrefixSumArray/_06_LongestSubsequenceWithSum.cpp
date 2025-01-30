f6c18634771fbcc0f53660a9d5d55368cf78545a
prince <princekumar7320918928@gmail.com>
2025-01-30 10:30:03 +0530

    upd

---
+++ _09_SlidingWindow&TwoPointer/_00_PW_PrefixSumArray/_06_LongestSubsequenceWithSum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+        int n=nums.size();
+        int m=queries.size();
+        sort(nums.begin(),nums.end());
+        vector<int> prefSum(n,0);
+        prefSum[0]=nums[0];
+        for(int i=1;i<n;i++) prefSum[i]=prefSum[i-1]+nums[i];
+        vector<int> ans(m,0);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(prefSum[j]<=queries[i]){
+                    ans[i]++;
+                }
+                else break;
+            }
+        }
+        return ans;
+    }
+};

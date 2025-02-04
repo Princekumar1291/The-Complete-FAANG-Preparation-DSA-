93c91ea88081f01d68f6db725afdbd4cd857a077
prince <princekumar7320918928@gmail.com>
2025-02-04 21:02:36 +0530

    upd

--- _14_DP/_05_DPonString/_04_printLongestSubsequence.cpp
+++ _14_DP/_05_DPonString/_04_printLongestSubsequence.cpp
@@ -1,32 +1,51 @@
-//Problem Link: https://www.naukri.com/code360/problems/print-longest-common-subsequence_8416383?leftPanelTabValue=PROBLEM
-#include<vector>
-#include<string>
-#include<algorithm>
+
+
+
+
+#include <iostream>
+#include <vector>
 using namespace std;
 
-string findLCS(int n,int m,string&s1,string&s2){
-    vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=m;j++){
-            if(s1[i-1]==s2[j-1]){
-                dp[i][j]=1+dp[i-1][j-1];
-            }else{
-                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+class Solution {
+public:
+    string longestCommonSubstr(string s1, string s2) {
+        int n = s1.size();
+        int m = s2.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        
+        int maxLength = 0, endIndex = -1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s1[i - 1] == s2[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                    if (dp[i][j] > maxLength) {
+                        maxLength = dp[i][j];
+                        endIndex = i - 1;  // Store the ending index in s1
+                    }
+                }
             }
         }
-    }
-    string ans="";
-    int i=n,j=m;
-    while(i>0&&j>0){
-        if(s1[i-1]==s2[j-1]){
-            ans=s1[i-1]+ans;
-            i--;
-            j--;
-        }else if(dp[i-1][j]>=dp[i][j-1]){
-            i--;
-        }else{
-            j--;
+
+        // Extract the longest common substring
+        string longestSubstring;
+        if (endIndex != -1) {
+            longestSubstring = s1.substr(endIndex - maxLength + 1, maxLength);
+        } else {
+            longestSubstring = "";
         }
+
+        cout << "Longest Common Substring: " << longestSubstring << endl;
+        cout << "Length: " << maxLength << endl;
+        
+        return longestSubstring;
     }
-    return ans;
+};
+
+int main() {
+    Solution sol;
+    string s1 = "abcde";
+    string s2 = "abfce";
+    sol.longestCommonSubstr(s1, s2);
+    return 0;
 }

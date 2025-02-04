21f1f36a0226303e58ed1b7ee957c8bee89c298b
prince <princekumar7320918928@gmail.com>
2025-02-04 20:15:25 +0530

    upd

---
+++ _14_DP/_05_DPonString/_04_printLongestSubsequence.cpp
@@ -0,0 +1,32 @@
+//Problem Link: https://www.naukri.com/code360/problems/print-longest-common-subsequence_8416383?leftPanelTabValue=PROBLEM
+#include<vector>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+string findLCS(int n,int m,string&s1,string&s2){
+    vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(s1[i-1]==s2[j-1]){
+                dp[i][j]=1+dp[i-1][j-1];
+            }else{
+                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+    }
+    string ans="";
+    int i=n,j=m;
+    while(i>0&&j>0){
+        if(s1[i-1]==s2[j-1]){
+            ans=s1[i-1]+ans;
+            i--;
+            j--;
+        }else if(dp[i-1][j]>=dp[i][j-1]){
+            i--;
+        }else{
+            j--;
+        }
+    }
+    return ans;
+}
--- demo.cpp
+++ demo.cpp
@@ -1,15 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
-public:
-    int longestCommonSubsequence(string s1, string s2) {
-        int n=s1.size();
-        int m=s2.size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(s1[i-1]==s2[j-1]) dp[i][j]=1+dp[i-1][j-1];
-                else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+  public:
+    string longestCommonSubstr(string s1, string s2) {
+        int n = s1.size();
+        int m = s2.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        
+        int maxLength = 0, endIndex = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s1[i - 1] == s2[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                    if (dp[i][j] > maxLength) {
+                        maxLength = dp[i][j];
+                        endIndex = i; // Store end index of LCS in s1
+                    }
+                }
             }
         }
-        return dp[n][m];
+        
+        // Extract substring from s1
+        if (maxLength == 0) return "";
+        return s1.substr(endIndex - maxLength, maxLength);
     }
 };
+
+int main() {
+    Solution sol;
+    string s1 = "abcde";
+    string s2 = "abfde";
+    cout << "Longest Common Substring: " << sol.longestCommonSubstr(s1, s2) << endl;
+    return 0;
+}

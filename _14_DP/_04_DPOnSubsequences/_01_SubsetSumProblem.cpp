d14c5bae80eb51fc47d0b836562531b640bb50b0
prince <princekumar7320918928@gmail.com>
2025-06-20 08:10:44 +0530

    upd

--- _14_DP/_04_DPOnSubsequences/_01_SubsetSumProblem.cpp
+++ _14_DP/_04_DPOnSubsequences/_01_SubsetSumProblem.cpp
@@ -1,23 +1,21 @@
 //Problem Link: https://www.geeksforgeeks.org/problems/subset-sum-problem-1611555638/1
 
-
-// class Solution{   
-// public:
-//     bool helper(vector<int>& arr,int target,int i,vector<vector<int>>& dp){
-//         if(target==0) return true;
-//         if(i==0) return target==arr[0];
-//         if(dp[i][target]!=-1) return dp[i][target];
-//         bool notTake=helper(arr,target,i-1,dp);
-//         bool take=false;
-//         if(arr[i]<=target) take=helper(arr,target-arr[i],i-1,dp);
-//         return dp[i][target]=notTake||take;
-//     }
-//     bool isSubsetSum(vector<int>arr, int target){
-//         int n=arr.size();
-//         vector<vector<int>> dp(n,vector<int>(target+1,-1));
-//         return helper(arr,target,n-1,dp);
-//     }
-// };
+class Solution {
+  public:
+    bool helper(vector<int>& arr,int n,int target,vector<vector<int>>& dp){
+        if(target==0) return true;
+        if(n<0 || target<0) return false;
+        if(dp[n][target]!=-1) return dp[n][target];
+        bool take=helper(arr,n-1,target-arr[n],dp);
+        bool notTake=helper(arr,n-1,target,dp);
+        return dp[n][target]=take||notTake;
+    }
+    bool isSubsetSum(vector<int>& arr, int target) {
+        int n=arr.size();
+        vector<vector<int>> dp(n,vector<int>(target+1,-1));
+        return helper(arr,n-1,target,dp);
+    }
+};
 
 
 class Solution {
--- _14_DP/_04_DPOnSubsequences/_08_TargetSum.cpp
+++ _14_DP/_04_DPOnSubsequences/_08_TargetSum.cpp
@@ -2,32 +2,6 @@
 
 
 
-
-class Solution {
-public:
-    int helper(vector<int>& nums,int target,int i,vector<vector<int>>& dp){
-        if(i==0){
-            if(target==0 && nums[0]==0) return 2;
-            if(target==nums[0] || target==-nums[0]) return 1;
-            return 0;
-        }
-        if(dp[i][target+2000]!=-1) return dp[i][target+2000];
-        int add=helper(nums,target+nums[i],i-1,dp);
-        int subt=helper(nums,target-nums[i],i-1,dp);
-        return dp[i][target+2000]=add+subt;
-    }
-    int findTargetSumWays(vector<int>& nums, int target) {
-        int n=nums.size();
-        vector<vector<int>> dp(n,vector<int>(4002,-1));
-        return helper(nums,target,n-1,dp);
-    }
-};
-
-
-
-
-
-
 class Solution {
 public:
     int helper(vector<int>& nums,int target,int n,vector<vector<int>>& dp){
@@ -36,19 +10,19 @@ public:
             if(target==0) return 1;
             else return 0;
         }
-        if(dp[n][target+10001]!=-1) return dp[n][target+10001];
+        if(dp[n][target]!=-1) return dp[n][target];
         int take=helper(nums,target-nums[n],n-1,dp);
         int notTake=helper(nums,target,n-1,dp);
-        return dp[n][target+10001]=take+notTake;
+        return dp[n][target]=take+notTake;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n=nums.size();
         int sum=0;
         for(int ele:nums) sum+=ele;
-        sum+=target;
-        if(sum%2==1) return 0;
+        sum-=target;
+        if(sum%2==1 || sum<0) return 0;
         sum/=2;
-        vector<vector<int>> dp(n,vector<int>(20004,-1));
+        vector<vector<int>> dp(n,vector<int>(sum+1,-1));
         return helper(nums,sum,n-1,dp);
     }
 };
